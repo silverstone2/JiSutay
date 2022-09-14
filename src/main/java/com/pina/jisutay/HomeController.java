a8ce69e968e8b57fb66ca7fb12298cc28433e9aa
silverstone2 <kcles21@naver.com>
2022-09-14 14:53:32 +0900

    create project

---
+++ src/main/java/com/pina/jisutay/HomeController.java
@@ -0,0 +1,18 @@
+package com.pina.jisutay;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home.do")
+	public String home(HttpServletRequest request) {
+		
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>/views/home.jsp</title>
+</head>
+<body>
+	<h1>인덱스 페이지 테스트중</h1>
+</body>
+</html>
